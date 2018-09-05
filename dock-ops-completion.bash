cf3066e3fa28ff1ad47ceed8dc52ce0364ac8d0c
Tyson Acker <tyson@devmode.com>
2018-09-05 07:16:59 -0500

    Recursive completion flag action

--- dock-ops-completion.bash
+++ dock-ops-completion.bash
@@ -3,54 +3,54 @@
 #
 # . ~/dock-ops-completion.bash
 
+skip_flags() {
+  case "$2" in
+    -m|-nc|-nd|-nm|--compose|--docker|--machine)
+      shift; shift
+      cmd=$(skip_flags "$@")
+      echo "$cmd"
+      ;;
+    -p|--production)
+      shift
+      cmd=$(skip_flags "$@")
+      echo "$cmd"
+      ;;
+    *)
+      echo "$2"
+      ;;
+  esac
+}
+
 __dock() {
-    local cur prev opts base
+    local cur prev CMD
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    cmd="${COMP_WORDS[1]}"
-
-    # skip over prefix flags to find command...
-    for ((i = 1; i < ${#COMP_WORDS[@]}; ++i))
-      do
-        case "${COMP_WORDS[i]}" in
-          -m|-nc|-nd|-nm|--compose|--docker|--machine)
-            cmd="${COMP_WORDS[i+2]}"
-            break
-            ;;
-          -p|--production)
-            cmd="${COMP_WORDS[i+1]}"
-            break
-            ;;
-          *)
-          ;;
-        esac
-    done
-
-    opts="build clean config down images logs ls ps push pull rls rmi run scp setup ssh stop tag up" # Basic commands to complete
+    CMD=$(skip_flags "${COMP_WORDS[@]}")
 
     # Complete the arguments to specified commands...
-    case "${cmd}" in
+    case "${CMD}" in
         build|run|up)
-            local names=`dock services`
-            COMPREPLY=( $(compgen -W "${names}" -- ${cur}) )
+            local services=`dock services`
+            COMPREPLY=( $(compgen -W "${services}" -- ${cur}) )
             return 0
             ;;
         logs|stop)
-            local names=`docker ps --format "{{.Names}}"`
-            COMPREPLY=( $(compgen -W "${names}" -- ${cur}) )
+            local containers=`docker ps --format "{{.Names}}"`
+            COMPREPLY=( $(compgen -W "${containers}" -- ${cur}) )
             return 0
             ;;
         use)
-            local names=`docker-machine ls --format "{{.Name}}"`
-            COMPREPLY=( $(compgen -W "${names}" -- ${cur}) )
+            local machines=`docker-machine ls --format "{{.Name}}"`
+            COMPREPLY=( $(compgen -W "${machines}" -- ${cur}) )
             return 0
             ;;
         *)
         ;;
     esac
 
-   COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
-   return 0
+    local commands=`dock commands`
+    COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
+    return 0
 }
 complete -F __dock dock
