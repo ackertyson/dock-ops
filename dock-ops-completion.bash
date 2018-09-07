509012cd71986063c40f74c7e466d67c0b49b547
Tyson Acker <tyson@devmode.com>
2018-09-07 15:29:27 -0500

    Tidy up completion script a bit

--- dock-ops-completion.bash
+++ dock-ops-completion.bash
@@ -22,6 +22,62 @@ skip_flags() {
   esac
 }
 
+__gitcomp () { # guess where I stole this from?
+    local cur_="${3-$cur}"
+
+    case "$cur_" in
+    --*=)
+        ;;
+    *)
+        local c i=0 IFS=$' \t\n'
+        for c in $1; do
+            c="$c${4-}"
+            if [[ $c == "$cur_"* ]]; then
+                case $c in
+                --*=*|*.) ;;
+                *) c="$c " ;;
+                esac
+                COMPREPLY[i++]="${2-}$c"
+            fi
+        done
+        ;;
+    esac
+}
+
+__commands() {
+  local cur="$1"
+  local commands=`dock commands`
+  COMPREPLY=( $(compgen -W "$commands" $cur) )
+}
+
+__containers() {
+  local cur="$1"
+  local containers=`docker ps --format "{{.Names}}"`
+  COMPREPLY=( $(compgen -W "$containers" $cur) )
+}
+
+__images() {
+  # requires more hand-holding because of the ':' in completion words
+  # (also requires bash-completion package)
+  local cur="$1"
+  local images=`docker images --format "{{.Repository}}:{{.Tag}}"`
+  _get_comp_words_by_ref -n : cur
+  COMPREPLY=( $(compgen -W "$images" $cur) )
+  __ltrim_colon_completions $cur
+}
+
+__machines() {
+  local cur="$1"
+  local machines=`docker-machine ls --format "{{.Name}}"`
+  COMPREPLY=( $(compgen -W "$machines" $cur) )
+}
+
+__services() {
+  local cur="$1"
+  local services=`dock services`
+  COMPREPLY=( $(compgen -W "$services" $cur) )
+}
+
 __dock() {
     local cur prev base
     COMPREPLY=()
@@ -42,35 +98,26 @@ __dock() {
     # Complete the arguments to specified commands...
     case "${base}" in
         build|run|up)
-            local services=`dock services`
-            COMPREPLY=( $(compgen -W "${services}" -- ${cur}) )
+            __services "$cur"
             return 0
             ;;
         logs|stop)
-            local containers=`docker ps --format "{{.Names}}"`
-            COMPREPLY=( $(compgen -W "${containers}" -- ${cur}) )
+            __containers "$cur"
             return 0
             ;;
         push|rmi|tag)
-            # requires more hand-holding because of the ':' in completion words
-            # (also requires bash-completion package)
-            local images=`docker images --format "{{.Repository}}:{{.Tag}}"`
-            _get_comp_words_by_ref -n : cur
-            COMPREPLY=( $(compgen -W "${images}" -- ${cur}) )
-            __ltrim_colon_completions ${cur}
+            __images "$cur"
             return 0
             ;;
         use)
-            local machines=`docker-machine ls --format "{{.Name}}"`
-            COMPREPLY=( $(compgen -W "${machines}" -- ${cur}) )
+            __machines "$cur"
             return 0
             ;;
         *)
         ;;
     esac
 
-    local commands=`dock commands`
-    COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
+    __commands "$cur"
     return 0
 }
 complete -F __dock dock
