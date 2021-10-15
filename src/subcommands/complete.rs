e219ced7363f5f1122adb7fe3534bef6ead8eb60
Tyson Acker <tyson@foundrymakes.com>
2021-10-15 15:52:08 -0500

    Avoid printing error during completion; tidying

--- src/subcommands/complete.rs
+++ src/subcommands/complete.rs
@@ -13,25 +13,30 @@ pub struct Complete {
 }
 
 fn strip_flags(args: &Vec<&str>) -> Vec<String> {
-    args.iter().filter(|arg| !arg.starts_with('-')).map(|s| s.to_string()).collect()
+    args.iter()
+        .filter(|arg| !arg.starts_with('-'))
+        .map(|s| s.to_string())
+        .collect()
 }
 
 pub fn complete(Complete { arg }: &Complete) -> Result<()> {
+    // remove flags/options so they don't F up our math
     let mut args = strip_flags(&arg.split(' ').collect::<Vec<_>>());
     let cmds = args.splice(..1, crate::vec_of_strings![]).collect::<Vec<_>>();
     let cmd: &str = cmds.get(0).unwrap();
+
     match args.len() {
         0 => { // $ dock <empty_or_partial_subcommand>_
-            // TODO missing YAML pushes "no such file" error into cmd completions xD
             let AppConfig { aliases, .. } = get(&String::from("development.json"))?;
             let builtins = crate::vec_of_strings![
                 "alias", "aliases", "attach", "build", "config", "down", "exec", "images", "logs",
                 "ps", "psa", "restart", "rmi", "run", "setup", "up"
             ];
-            let all = concat(builtins, aliases.keys().map(String::to_owned).collect());
+            let all = concat(
+                builtins,
+                aliases.keys().map(String::to_owned).collect());
             Ok(io::stdout().write_all(all.join(" ").as_bytes())?)
         },
-        // TODO accommodate subcommand options: $ dock <subcommand> <options> _
         1 => match cmd { // $ dock <subcommand> _
             "attach" | "stop" => {
                 Ok(io::stdout().write_all(&completion_containers()?)?)
